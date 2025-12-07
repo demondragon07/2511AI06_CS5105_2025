00dddf13936bd2dde4cae5745b980e146847bb62
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-12-07 13:31:20 +0530

    Move Arrays ,DP ,Graph into Codes folder

--- Codes/Arrays/P001.cpp
+++ Codes/Arrays/P001.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-
-    return 0;
-}
+int brute_max_element(vector<int>& a){int mx=INT_MIN; for(int x:a) mx=max(mx,x); return mx;}
+int optimal_max_element(vector<int>& a){return *max_element(a.begin(), a.end());}
+int main(){return 0;}
